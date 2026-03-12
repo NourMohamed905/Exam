efbafd6c29c209f5f26c2444ea347d960790c941
omar <omarxameen@gmail.com>
2026-03-12 07:30:49 +0200

    feat: setup core architecture and project base
    - Add routing setup (app_routes, route_generator)
    - Add core utilities (error_handler, font_manager, values_manager)
    - Add auth local storage
    - Add toast message and placeholder widget
    - Update dependencies (google_fonts,flutter_secure_storage,flutter_screenutil,fluttertoast,dio)

---
+++ lib/config/routes/route_generator.dart
@@ -0,0 +1,45 @@
+import 'package:exam_app/core/widgets/place_holder_widget.dart';
+import 'package:flutter/material.dart';
+
+import 'app_routes.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.login:
+        // Todo: Replace (Placeholder) With Your Screen .
+        return MaterialPageRoute(
+          builder: (_) => const PlaceHolderWidget(screenName: 'Login Screen'),
+        );
+
+      case AppRoutes.signup:
+        return MaterialPageRoute(
+          builder: (_) => const PlaceHolderWidget(screenName: 'Signup Screen'),
+        );
+
+      case AppRoutes.forgotPassword:
+        return MaterialPageRoute(
+          builder: (_) =>
+              const PlaceHolderWidget(screenName: 'Forgot Password Screen'),
+        );
+
+      case AppRoutes.verificationCode:
+        return MaterialPageRoute(
+          builder: (_) =>
+              const PlaceHolderWidget(screenName: 'Verification Code Screen'),
+        );
+
+      case AppRoutes.resetPassword:
+        return MaterialPageRoute(
+          builder: (_) =>
+              const PlaceHolderWidget(screenName: 'Reset Password Screen'),
+        );
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) =>
+              const Scaffold(body: Center(child: Text("No route found"))),
+        );
+    }
+  }
+}
