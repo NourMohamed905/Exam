8b0545754db8b833f70b2cd9c545154115589f9d
Omar Ameen <omarxwork10@gmail.com>
2026-03-13 04:32:22 +0200

    feat(api): implement auth api endpoints .

--- lib/api/auth/auth_api.dart
+++ lib/api/auth/auth_api.dart
@@ -1,50 +1,34 @@
 import 'package:dio/dio.dart';
 import 'package:exam_app/core/constants/app_endpoints.dart';
+import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'auth_api.g.dart';
 
 @RestApi(baseUrl: AppEndPoints.baseUrl)
+@Injectable()
 abstract class AuthApi {
-
-  factory AuthApi(Dio dio, {String baseUrl}) = _AuthApi;
+  @factoryMethod
+  factory AuthApi(Dio dio) = _AuthApi;
 
   @POST(AppEndPoints.signup)
-  Future<HttpResponse> signup(
-    @Body() Map<String, dynamic> body,
-  );
+  Future<HttpResponse> signup(@Body() Map<String, dynamic> body);
   @POST(AppEndPoints.login)
-  Future<HttpResponse> login(
-    @Body() Map<String, dynamic> body,
-  );
-  @PATCH( AppEndPoints.changePassword)
-  Future<HttpResponse> changePassword(
-    @Body() Map<String, dynamic> body,
-  );
+  Future<HttpResponse> login(@Body() Map<String, dynamic> body);
+  @PATCH(AppEndPoints.changePassword)
+  Future<HttpResponse> changePassword(@Body() Map<String, dynamic> body);
   @DELETE(AppEndPoints.deleteAccount)
   Future<HttpResponse> deleteAccount();
   @PATCH(AppEndPoints.editProfile)
-  Future<HttpResponse> editProfile(
-    @Body() Map<String, dynamic> body,
-  );
+  Future<HttpResponse> editProfile(@Body() Map<String, dynamic> body);
   @POST(AppEndPoints.logout)
-  Future<HttpResponse> logout(
-    @Body() Map<String, dynamic> body,
-  );
+  Future<HttpResponse> logout(@Body() Map<String, dynamic> body);
   @GET(AppEndPoints.getUserInfo)
-  Future<HttpResponse> getUserInfo(
-    @Body() Map<String, dynamic> body,
-  );
+  Future<HttpResponse> getUserInfo(@Body() Map<String, dynamic> body);
   @POST(AppEndPoints.forgotPassword)
-  Future<HttpResponse> forgotPassword(
-    @Body() Map<String, dynamic> body,
-  );
+  Future<HttpResponse> forgotPassword(@Body() Map<String, dynamic> body);
   @POST(AppEndPoints.verifyResetCode)
-  Future<HttpResponse> verifyResetCode(
-    @Body() Map<String, dynamic> body,
-  );
+  Future<HttpResponse> verifyResetCode(@Body() Map<String, dynamic> body);
   @POST(AppEndPoints.resetPassword)
-  Future<HttpResponse> resetPassword(
-    @Body() Map<String, dynamic> body,
-  );
+  Future<HttpResponse> resetPassword(@Body() Map<String, dynamic> body);
 }
