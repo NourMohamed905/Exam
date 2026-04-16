73339d1df4fdb81c8a7ba043bb67f33a0db6c96e
Omar Ameen <omarxwork10@gmail.com>
2026-04-16 14:55:10 +0200

    feat: add profile domain entities and use cases for user info and profile updates

---
+++ lib/features/profile/domain/repository/profile_repo_contract.dart
@@ -0,0 +1,11 @@
+import 'package:exam_app/config/base_response/base_response.dart';
+import 'package:exam_app/features/profile/domain/entity/get_user_info_response.dart';
+import 'package:exam_app/features/profile/domain/entity/request/update_profile_request.dart';
+import 'package:exam_app/features/profile/domain/entity/update_profile_response.dart';
+
+abstract class ProfileRepoContract {
+  Future<BaseResponse<GetUserInfoResponse>> getUserInfo();
+  Future<BaseResponse<UpdateProfileResponse>> updateProfile(
+    UpdateProfileRequest request,
+  );
+}
