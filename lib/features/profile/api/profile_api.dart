ef508699077b9046dffd460df0e301b4bb0e4459
yassmena <>
2026-04-23 01:51:11 +0200

    UI

--- lib/features/profile/api/profile_api.dart
+++ lib/features/profile/api/profile_api.dart
@@ -1,11 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:exam_app/core/constants/api_param.dart';
 import 'package:exam_app/core/constants/app_endpoints.dart';
-import 'package:exam_app/features/home/home_subject/data/model/subject_response_dto.dart';
 import 'package:exam_app/features/profile/data/model/get_user_info_response_dto.dart';
 import 'package:exam_app/features/profile/data/model/request/update_profile_request_dto.dart';
 import 'package:exam_app/features/profile/data/model/update_profile_response_dto.dart';
-import 'package:exam_app/features/profile/domain/entity/request/update_profile_request.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 
